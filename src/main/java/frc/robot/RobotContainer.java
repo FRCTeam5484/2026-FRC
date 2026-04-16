1614a35bf0efbb693414c2f0bcb9a93d1ae423f7
rimabbell <rimabbell@gmail.com>
2026-04-16 18:20:11 -0400

    Auto improvements

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -73,7 +73,7 @@ public class RobotContainer {
         NamedCommands.registerCommand("Auto Shoot", new cmdAuto_AutoShoot(hood, shooter, bed, feeder).withTimeout(4));
         NamedCommands.registerCommand("Climb Raise Auto", new cmdClimb_Raise(climb).withTimeout(3));
         NamedCommands.registerCommand("Climb Lower Auto", new cmdClimb_Lower(climb).withTimeout(4));
-        NamedCommands.registerCommand("Auto Align Shoot and Move", new cmdAuto_AutoAlignShootMove(drivetrain, hood, shooter, bed, feeder, intake, ()->0.2).withTimeout(5));
+        NamedCommands.registerCommand("Auto Align Shoot and Move", new cmdAuto_AutoAlignShootMove(drivetrain, hood, shooter, bed, feeder, intake, ()->0.2).withTimeout(2.5));
         NamedCommands.registerCommand("Auto Extend Hopper", new cmdHopper_Extend(hopper).withTimeout(2));
         NamedCommands.registerCommand("Auto Retract Hopper", new cmdHopper_Retract(hopper).withTimeout(2));
         NamedCommands.registerCommand("Auto Run Intake", new cmdIntake_TeleOp(intake, ()->0.7).withTimeout(5));
